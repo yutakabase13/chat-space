88e5c8a95e54561431aea64f201e7f79af1e731a
yutakabase13 <shd.ytk1224@gmail.com>
2020-07-08 15:14:20 +0900

    Chat-space-ajax

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.0.beta2'
+  gem 'rails-controller-testing'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
@@ -51,10 +55,11 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem "haml-rails", " 1.0", '<= 2.0.1'
-gem 'font-awesome-sass'
-gem 'devise'
-gem 'pry-rails'
-gem 'carrierwave'
-gem 'mini_magick'
+  gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+  gem "haml-rails", " 1.0", '<= 2.0.1'
+  gem 'font-awesome-sass'
+  gem 'devise'
+  gem 'pry-rails'
+  gem 'carrierwave'
+  gem 'mini_magick'
+  gem 'jquery-rails'
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+          `<div class="main-chat__message__contents">
+            <div class="main-chat__message__contents__post">
+              <div class="main-chat__message__contents__name">
+                ${message.user_name}
+              <div class="main-chat__message__contents__time">
+                ${message.created_at}
+                </div>
+              </div>
+            </div>
+            <div class=".main-chat__form__format">
+              <p class="main-chat__message__contents__comment">
+                ${message.content}
+              </p>
+              <img class="Message__image" src="${message.image}">
+            </div>
+          </div>`
+      return html;
+    } else {
+      let html =
+          `<div class="main-chat__message__contents">
+            <div class="main-chat__message__contents__post">
+              <div class="main-chat__message__contents__name">
+                ${message.user_name}
+            <div class="main-chat__message__contents__time">
+                ${message.created_at}
+                </div>
+              </div>
+            </div>
+            <div class="main-chat__form__format">
+              <p class="main-chat__message__contents__comment">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+      return html;
+    };
+  }
+
+  $('.main-chat__form__format').on('submit', function(e){
+    e.preventDefault();   
+      let formData = new FormData(this);
+      let url = $(this).attr('action');
+      $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.main-chat__message').append(html);
+      $('form')[0].reset();
+      $('.main-chat__message').animate({ scrollTop: $('.main-chat__message')[0].scrollHeight});
+      $('.submit-btn').prop('disabled', false);
+   })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました")
+   });
+  })
+});
